c9c5b4a834f050c91798fbf1c613e792610f78ef
Silas Jaworowicz <125sjaworowicz@frhsd.com>
2024-09-10 10:54:40 -0400

    8:56

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//board
+let board;
+let boardWidth = 360;
+let boardHeight = 576;
+let context;
+
+//doodler
+let doodlerWidth = 46;
+let doodlerHeight = 46;
+let doodlerX = boardWidth/2 - doodlerWidth/2;
+let doodlerY = boardHeight*7/8 - doodlerHeight;
+let doodlerRightImg;
+let doodlerLeftImg;
+
+let doodler = {
+    img : null,
+    x : doodlerX,
+    y : doodlerY
+    width : doodlerWidth,
+    height : doodlerHeight,
+}
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d"); //used for drawing on the board
+
+    //draw doodler
+}
