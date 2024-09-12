c06a9c75679c6ecca0650aded2ae6760190f97d0
Silas Jaworowicz <125sjaworowicz@frhsd.com>
2024-09-12 08:18:36 -0400

    15:48

--- js/script.js
+++ js/script.js
@@ -12,10 +12,13 @@ let doodlerY = boardHeight*7/8 - doodlerHeight;
 let doodlerRightImg;
 let doodlerLeftImg;
 
+//physics
+let velocityX = 0;
+
 let doodler = {
     img : null,
     x : doodlerX,
-    y : doodlerY
+    y : doodlerY,
     width : doodlerWidth,
     height : doodlerHeight,
 }
@@ -27,4 +30,38 @@ window.onload = function() {
     context = board.getContext("2d"); //used for drawing on the board
 
     //draw doodler
+    //context.fillStyle = "green";
+    //context.fillRect(doodler.x, doodler.y, doodler.width, doodler.height);
+
+    //load images
+    doodlerRightImg = new Image();
+    doodlerRightImg.src = "img/doodler-right.png";
+    doodler.img = doodlerRightImg;
+    doodlerRightImg.onload = function() {
+        context.drawImage(doodler.img, doodler.x, doodler.y, doodler.width, doodler.height);
+    }
+
+    doodlerLeftImg = new Image();
+    doodlerLeftImg.src = "img/doodler-left.png";
+
+    requestAnimationFrame(update);
+    document.addEventListener("keydown", moveDoodler);
 }
+
+function update() {
+    requestAnimationFrame(update);
+
+    //doodler
+    context.drawImage(doodler.img, doodler.x, doodler.y, doodler.width, doodler.height);
+}
+
+function moveDoodler(e) {
+    if(e.code == "ArrowRight" || e.code == "KeyD") { //move right
+        velocityX = 4;
+        doodler.img = doodlerRightImg;
+    }
+    else if(e.code == "ArrowLeft" || e.code == "KeyA") { //move left
+
+    }
+}
+
