9b43a2e965754e024f16716ff758018f47b0c42e
Silas Jaworowicz <125sjaworowicz@frhsd.com>
2024-09-17 08:19:08 -0400

    Final product

--- js/script.js
+++ js/script.js
@@ -12,6 +12,14 @@ let doodlerY = boardHeight*7/8 - doodlerHeight;
 let doodlerRightImg;
 let doodlerLeftImg;
 
+let doodler = {
+    img : null,
+    x : doodlerX,
+    y : doodlerY,
+    width : doodlerWidth,
+    height : doodlerHeight,
+}
+
 //physics
 let velocityX = 0;
 let velocityY = 0; //doodler jump speed
@@ -24,13 +32,9 @@ let platformWidth = 60;
 let platformHeight = 18;
 let platformImg;
 
-let doodler = {
-    img : null,
-    x : doodlerX,
-    y : doodlerY,
-    width : doodlerWidth,
-    height : doodlerHeight,
-}
+let score = 0;
+let maxScore = 0;
+let gameOver = false;
 
 window.onload = function() {
     board = document.getElementById("board");
@@ -64,6 +68,9 @@ window.onload = function() {
 
 function update() {
     requestAnimationFrame(update);
+    if (gameOver) {
+        return;
+    }
     context.clearRect(0, 0, board.width, board.height);
 
     //doodler
@@ -77,15 +84,37 @@ function update() {
 
     velocityY += gravity;
     doodler.y += velocityY;
+    if (doodler.y > board.height) {
+        gameOver = true;
+    }
     context.drawImage(doodler.img, doodler.x, doodler.y, doodler.width, doodler.height);
 
     //platforms
     for (let i = 0; i < platformArray.length; i++) {
         let platform = platformArray[i];
+        if (velocityY < 0 && doodler.y < boardHeight*3/4) {
+            platform.y -= initialVelocityY; //slide platform down
+        }
         if (detectCollision(doodler, platform)&& velocityY >= 0) {
             velocityY = initialVelocityY; //jump
         }
         context.drawImage(platform.img, platform.x, platform.y, platform.width, platform.height);
+    }
+
+    //clear platforms and add new platform
+    while (platformArray.length > 0 && platformArray[0].y >= boardHeight) {
+        platformArray.shift(); //removes first element from the array
+        newPlatform(); //replace with new platform at the top
+    }
+
+    //score
+    updateScore();
+    context.fillStyle = "black";
+    context.font = "16px sans-serif";
+    context.fillText(score, 5, 20);
+
+    if (gameOver) {
+        context.fillText("Game Over: Press 'Space' to Restart", boardWidth/7, boardHeight*7/8);
     }
 }
 
@@ -97,6 +126,23 @@ function moveDoodler(e) {
     else if(e.code == "ArrowLeft" || e.code == "KeyA") { //move left
         velocityX = -4;
         doodler.img = doodlerLeftImg;
+    }
+    else if (e.code == "Space" && gameOver) {
+        //reset
+        doodler = {
+            img : doodlerRightImg,
+            x : doodlerX,
+            y : doodlerY,
+            width : doodlerWidth,
+            height : doodlerHeight,
+        }
+
+        velocityX  = 0;
+        velocityY = initialVelocityY;
+        score = 0;
+        maxScore = 0;
+        gameOver = false;
+        placePlatforms();
     }
 }
 
@@ -138,7 +184,16 @@ function placePlatforms() {
 }
 
 function newPlatform() {
+    let randomX = Math.floor(Math.random() * boardWidth*3/4); //(0-1) * boardWidth*3/4
+        let platform = {
+            img : platformImg,
+            x : randomX,
+            y : -platformHeight,
+            width : platformWidth,
+            height : platformHeight
+        }
     
+        platformArray.push(platform);
 }
 
 function detectCollision(a, b) {
@@ -146,4 +201,17 @@ function detectCollision(a, b) {
     a.x + a.width > b.x && //a's top right corner passes b's top left corner
     a.y < b.y + b.height && //a's top left corner doesn't reach b's bottom left corner
     a.y + a.height > b.y; //a's bottom left corner passes b's top left corner
+}
+
+function updateScore() {
+    let points = Math.floor(50*Math.random()); //(0-1) *50 --> (0-50)
+    if (velocityY < 0) { //negative going up
+        maxScore += points;
+        if(score < maxScore) {
+            score = maxScore;
+        }
+    }
+    else if (velocityY >= 0) {
+        maxScore -= points;
+    }
 }
